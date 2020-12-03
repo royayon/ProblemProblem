448be3c703ebb218d9e95346057bfcfc0234e60e
Ayon Roy <royshouhag@gmail.com>
2020-12-03 15:20:39 +0600

    script of jupyter added

--- Anaconda/Script_JupyterNotebook.bat
+++ Anaconda/Script_JupyterNotebook.bat
@@ -1,3 +1,7 @@
+echo Ayon Jupyter Notebook Script
 call "C:\ProgramData\Anaconda3\Scripts\activate.bat"
 cd %CD%
+call conda env list
+call set /p env="Enter environment name: " 
+call conda activate %env%
 jupyter notebook
